c2a5b11d3456cbae365cf1ac17f456ec22b85bce
Jennyx-1984 <163904901+Jennyx-1984@users.noreply.github.com>
2026-01-11 17:11:36 +0100

    feat: added json-server, axios library, register form, login form, header, confirmation message with info from user, router, services (checks) , json server calling with axios and styles

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router";
+import Header from "../components/organisms/Header/Header";
+
+const Layout = () => {
+    return (
+        <>
+            <Header/>
+            <main><Outlet/></main>
+        </>
+    )
+}
+
+export default Layout
