09d4f51246c611ce41857281b8bd4a468a98eb2e
Guadalupe <penaegeaguadalupe@gmail.com>
2026-01-14 12:37:48 +0100

    fix: identation ok in Start

--- src/pages/Start/Start.jsx
+++ src/pages/Start/Start.jsx
@@ -4,13 +4,13 @@ import BoardCards from '../../components/organisms/BoardCards/BoardCards';
 import styles from "./start.module.css";
 
 const Start = () => {
-    
-    
+
+
     return (
         <>
-             <div className={styles.start_overflow}>  
-            <h2 className={styles.title_start}>Elige 3 cartas para el orden de pasado, presente y futuro</h2>
-            <BoardCards/>
+            <div className={styles.start_overflow}>
+                <h2 className={styles.title_start}>Elige 3 cartas para el orden de pasado, presente y futuro</h2>
+                <BoardCards />
             </div>
         </>
     );
