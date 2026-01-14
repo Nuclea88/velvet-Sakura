785301b8d9e87d783534c75591e9a9d3d0587217
Nuclea88 <ingrid.lopov@gmail.com>
2026-01-14 14:51:35 +0100

    feat: apllying checkButton in reading card

--- src/components/molecules/ReadingCard/ReadingCard.jsx
+++ src/components/molecules/ReadingCard/ReadingCard.jsx
@@ -2,10 +2,25 @@ import DeleteButton from "../deleteButton.jsx/deleteButton.jsx";
 import EditButton from "../../atoms/editButton/EditButton.jsx";
 import styles from "./reading-card.module.css";
 import { useState } from "react";
+import CheckButton from "../../atoms/checkButton/CheckButton.jsx";
+import apiReading from "../../../services/apiReading.jsx";
 
 const ReadingCard = ({date, name, id, onDelete}) =>{
     const [isEditing, setIsEditing] = useState(false);
     const [tempName, setTempName] = useState(name);
+
+    const db = apiReading();
+
+    const handleSave = () => {
+        db.editName(id, tempName).then(() => {
+        setIsEditing(false)
+        })
+        .catch((err) => {
+                console.error("Error al actualizar:", err);
+                alert("No se pudo guardar el cambio.");
+            });
+    };
+
     return( 
        <div className={styles.cardContainer}>
             <img src="src/assets/images/historial.png" alt="iconoHistorial"></img>
@@ -14,19 +29,17 @@ const ReadingCard = ({date, name, id, onDelete}) =>{
                 {isEditing ? (
                 <>
                     <input 
+                        className={styles.editInput}
                         value={tempName} 
                         onChange={(event) => setTempName(event.target.value)} 
+                        autofocus
                     />
-                    {/* El botón de confirmar (Check) va aquí fuera */}
-                    <button onClick={() => {
-                        console.log("Guardando...", tempName);
-                        setIsEditing(false);
-                    }}>✅</button>
+                    <CheckButton onSave={handleSave} />     
                 </>
             ) : (
                 <>
                 <EditButton onOpenEdit={() => setIsEditing(true)}/> 
-                <span>{name}</span>
+                <span>{tempName}</span>
                 </>)}
             </div>
             <DeleteButton id={id} onDelete={onDelete} /> 
