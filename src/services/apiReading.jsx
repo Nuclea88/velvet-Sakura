bad0c09efa32e1be6ea7859ca060c9d4cca9e95e
Jennyx-1984 <163904901+Jennyx-1984@users.noreply.github.com>
2026-01-16 11:31:53 +0100

    update: no comments

--- src/services/apiReading.jsx
+++ src/services/apiReading.jsx
@@ -2,11 +2,6 @@ import axios from "axios";
 
 const apiReading=()=>{
     const url="http://localhost:3000/readings";
-    
-
-    /*const addReading = async (dataForm) => {
-        const response = await axios.post(url, dataForm);
-        return response.data;*/
 
     const getByUserId = async (userId) => {
         const response = await axios.get(`${url}?userId=${userId}`);
